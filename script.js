14574e28cda5cd0acc9cd21280b172c813f15684
Yorqinovs <sunnatilloyorqinov2830@gmail.com>
2023-06-04 19:24:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+//// Dinamic Typing - bu bitta malumot turidan boshqa malumot turiga o'tish
+
+//// to string \\\\
+//// 1-)
+// console.log(typeof String(28)); // string
+
+// //// 2-)
+// console.log(typeof ("Sunnatillo" + 28)); // string
+
+// const youtubeChannel = 10;
+// console.log("youtube.com/channel/" + youtubeChannel); // youtube.com/channel/10
+
+//// to number \\\\
+//// 1-)
+// console.log(typeof Number(28)); // number
+
+// //// 2-)
+// console.log(typeof +"28"); // number
+
+// //// 3-)
+// console.log(typeof parseInt("15"));
+
+//// to boolean \\\\
+//// 0, "", null, undefined, NaN = false (hardoim bizga false qaytaradi)
+//// 1-)
+// let age = 0;
+// if (age) {
+//   console.log("g");
+// }
+// age = 28;
+// if (age) {
+//   console.log("g");
+// }
+
+//// 2-)
+// console.log(typeof Boolean("15")); // boolean
+
+//// 2-)
+// console.log(typeof !!"15"); // boolean
